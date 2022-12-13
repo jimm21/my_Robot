86b51aa5e2612e6c1c110b806470e3b2367e1703
JoseManuel <jpachecog@uni.pe>
2022-12-13 03:15:50 +0000

    avance codigo pasa puntos

--- urdf_manipulator_simulate/src/pruebas2.py
+++ urdf_manipulator_simulate/src/pruebas2.py
@@ -5,6 +5,7 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 from math import pi
+from tf.transformations import quaternion_from_euler
 
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('trajectory_control', anonymous=True)
@@ -16,15 +17,38 @@ display_trajectory_publisher = rospy.Publisher(
     '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
 
 
-wpose = group.get_current_pose().pose
-print(wpose)
+current_pose = group.get_current_pose().pose
+print(current_pose)
 
 pose_goal = geometry_msgs.msg.Pose()
-pose_goal.orientation = wpose.orientation
-pose_goal.position.x = wpose.position.x
-pose_goal.position.y = wpose.position.y
-pose_goal.position.z = wpose.position.z
+# Pose Position
+x_coordinate = 0.288622765468
+y_coordinate = -0.199959113949
+z_coordinate = 0.0743749161064
+pose_goal.position.x = x_coordinate
+pose_goal.position.y = y_coordinate
+pose_goal.position.z = z_coordinate
+
+# Pose Orientation
+roll_angle = 1.59
+pitch_angle = -pi/2
+yaw_angle = pi
+quaternion = quaternion_from_euler(roll_angle, pitch_angle, yaw_angle, axes='szyz')
+print(quaternion)
+
+#pose_goal.orientation.w = quaternion[0]
+#pose_goal.orientation.x = quaternion[1]
+#pose_goal.orientation.y = quaternion[2]
+#pose_goal.orientation.z = quaternion[3]
+
+pose_goal.orientation.w = 0.481755087181
+pose_goal.orientation.x = -0.516807041881
+pose_goal.orientation.y = 0.482195311162
+pose_goal.orientation.z = -0.51798667872
+
+print(pose_goal)
 group.set_pose_target(pose_goal)
+
 plan = group.go(wait=True)
 # Calling `stop()` ensures that there is no residual movement
 group.stop()
